348eb250c8d8b0284ca5e59646c07b0ae12ad197
luan0731 <louisnguyen0731@gmail.com>
2017-03-18 16:29:18 -0700

    Revert "Added Box2D benchmark"
    This reverts commit b59cbe0ebfea991feca272431429400e68557c37.

--- Box2D/closure/compile.bat
+++ Box2D/closure/compile.bat
@@ -1,1 +1,1 @@
-java -jar ../../closure-compiler-v20170218.jar --js ../src/Box2D.js --js_output_file Box2D.closure.js
+java -jar ..\..\..\closure-compiler-v20170218.jar --compilation_level ADVANCED_OPTIMIZATIONS --js ..\src\Box2D.js --js_output_file Box2D.closure.js
